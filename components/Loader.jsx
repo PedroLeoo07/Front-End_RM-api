dbcf6959f55d2710e26db87730e21ff6ff1cde20
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-04-24 10:36:30 -0300

    Projeto finalizado

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/Loader.module.css";
     export default function Loader() {
         return (
             <div className={styles.container}>
-                <Image src={"/loader.gif"} alt="Loading 🙂" width={300} height={300} priority className={styles.mage} />
+                <Image src={"/loader.gif"} alt="Loading 🙂" width={300} height={300} priority className={styles.image} />
                 <h1 className={styles.message}>Carregando... Por favor aguarde!!</h1>
             </div>
         );
